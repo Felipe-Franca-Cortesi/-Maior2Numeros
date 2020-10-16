70af56dd84f00b5c0cfd5134ddce187bc37aa138
Felipe França <contatofelipefranca12@gmail.com>
2020-10-16 19:35:48 -0300

    Adiciona a conclusão do exercício Maior2Numeros

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroUm, numeroDois;
+            string textoNumeroUm, textoNumeroDois;
+            bool numeroUmValido, numeroDoisValido;
+
+            Console.Clear();
+            Console.WriteLine("--- Maior de dois números");
+
+            Console.Write("Digite o primeiro número..: ");
+            textoNumeroUm = Console.ReadLine();
+            Console.Write("Digite o segundo número...: ");
+            textoNumeroDois = Console.ReadLine();
+
+            numeroUmValido = double.TryParse(textoNumeroUm, out numeroUm);
+            numeroDoisValido = double.TryParse(textoNumeroDois, out numeroDois);
+
+            if (!numeroUmValido)
+            {
+               Console.WriteLine("Valor inválido");
+               Environment.Exit(-1);
+            }
+            else if (!numeroDoisValido)
+            {
+               Console.WriteLine("Valor inválido");
+               Environment.Exit(-1);
+            }
+            else if (numeroUm > numeroDois)
+            {
+                Console.WriteLine($"O maior número é {numeroUm}");
+            }
+            else
+            {
+                Console.WriteLine($"O maior número é {numeroDois}");
+            }
+            
+        }
+    }
+}
